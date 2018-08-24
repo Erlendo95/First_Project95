7b957e35d9383eefaa18c503958dcb2f79b3f291
Erlend Lemva Ousdal <er_lendo@hotmail.com>
2018-08-24 12:08:40 +0200

    first

---
+++ andre.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int square(int a){
+    return a*a;
+}
+
+int main() {
+    uint16_t a = square(5);
+    uint16_t b = 5;
+    cout << "Hello Worldsasdasdasdasda " << sizeof(a) << endl;
+}
+
